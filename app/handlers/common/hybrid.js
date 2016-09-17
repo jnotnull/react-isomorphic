736da7d27aec09435be5c45369c840e3583f59ec
季严亮 <jiyanliang@jiyanliangdeMacBook-Pro.local>
2016-09-17 11:39:40 +0800

    去掉无用代码

--- app/handlers/common/hybrid.js
+++ app/handlers/common/hybrid.js
@@ -20,58 +20,7 @@ var getUrl = function() {
   return url;
 }
 
-// 运行native代码
-var nativeFun = function(params) {
-  if (util.isMobile.Android()) {
-    params = JSON.stringify(params);
-    JSInterface.nativeFunction(params);
-  } else if (util.isMobile.iOS()) {
-    WebViewJavascriptBridge.send(params)
-  }
-}
-
-var getUserId = function() {
-  var defer = $.Deferred();
-
-  var userId;
-
-  if (util.isMobile.Android()) {
-    userId = JSInterface.getUserId();
-    defer.resolve(userId);
-  } else if (util.isMobile.iOS()) {
-    //ios获取userid
-
-    function connectWebViewJavascriptBridge(callback) {
-      if (window.WebViewJavascriptBridge) {
-        callback(WebViewJavascriptBridge)
-      } else {
-        document.addEventListener('WebViewJavascriptBridgeReady', function() {
-          callback(WebViewJavascriptBridge)
-        }, false)
-      }
-    }
-    connectWebViewJavascriptBridge(function(bridge) { /* Init your app here */
-      bridge.init(function(message, responseCallback) {
-        userId = message.UserId;
-        defer.resolve(userId);
-      })
-
-      bridge.registerHandler('onPageShow', function(data, responseCallback) {
-        userId = data.UserId;
-        defer.resolve(userId);
-      })
-
-      bridge.registerHandler('onPageHide', function(data, responseCallback) {
-
-      })
-    })
-  }
-
-  return defer.promise();
-}
-
 var hybrid = module.exports = {
   "getUrl": getUrl,
-  "nativeFun": nativeFun,
-  "getUserId": getUserId
+  "nativeFun": nativeFun
 }
